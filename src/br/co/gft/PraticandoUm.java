df58623db37dc991cbc98cef0fb66107c164789b
pkovbin <reginaazzi@hotmail.com>
2022-02-03 14:34:08 -0300

    first commit

---
+++ src/br/co/gft/PraticandoUm.java
@@ -0,0 +1,14 @@
+package br.co.gft;
+
+import br.co.gft.model.Gato;
+
+public class PraticandoUm {
+    public static void main(String[] args) {
+        Gato gato = new Gato();
+        System.out.println(gato);
+
+        /*int a = 5;
+        int b = 3;
+        System.out.println("hello word " + (a+b));*/
+    }
+}
